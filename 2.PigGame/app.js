03a2f1668c5b2c9f71134c28154b6983af3036b2
Ros1 <kamil.m.ros1@gmail.com>
2020-07-12 20:35:33 +0200

    Added two dices, field for points and double Six roll event

--- 2.PigGame/app.js
+++ 2.PigGame/app.js
@@ -1,21 +1,25 @@
 
-var dice, current,score1, score2, isPlayerOne, endGame;
-endGame =false;
+var dice, dice2, current, score1, score2, isPlayerOne, endGame;
+endGame = false;
+
 
 function playGame(){
-
+	
 	isPlayerOne=true;
-	dice=0;
+	dice=dice2=0;
 	current=0;
-	score1=0;
-	score2=2;
+	score1=10;
+	score2=10;
 
-	document.getElementById('current-0').innerHTML=0;
-	document.getElementById('current-1').innerHTML=0;
-	document.getElementById('score-0').innerHTML=0;
-	document.getElementById('score-1').innerHTML = 0;
+	document.getElementById('current-0').innerHTML = 0;
+	document.getElementById('current-1').innerHTML = 0;
+	document.getElementById('score-0').innerHTML = score1;
+	document.getElementById('score-1').innerHTML = score2;
+	document.getElementById("points").value = 100;
+
 	document.querySelector('.player-1-panel').classList.remove('active');
 	document.querySelector('.player-0-panel').classList.add('active');
+
 	document.getElementById("roll").addEventListener("click", rollDice);
 	document.getElementById("hold").addEventListener("click", hold);
 	document.getElementById("newGame").addEventListener("click", playGame);
@@ -23,28 +27,19 @@ function playGame(){
 
 function rollDice(){
 	
-	dice = Math.floor(Math.random()*(6)+1); 
+	dice = Math.floor(Math.random() * (6) + 1); 
+	dice2 = Math.floor(Math.random() * (6) + 1); 
+
+
 	changeImage();
-	current+=dice;
+	current+=(dice+dice2);
 
 	if(isPlayerOne)	document.getElementById('current-0').innerHTML=current;
 	else document.getElementById('current-1').innerHTML=current;
- 
-	if(dice == 1 && isPlayerOne) {
-		current = 0;
-		document.getElementById('current-0').innerHTML=current;
-		isPlayerOne =!isPlayerOne;
-		document.querySelector('.player-0-panel').classList.remove('active');
-		document.querySelector('.player-1-panel').classList.add('active');
-	}
-	else if (dice == 1 && !isPlayerOne)	{
-		current = 0;
-		document.getElementById('current-1').innerHTML=current;
-		isPlayerOne =!isPlayerOne;
-		document.querySelector('.player-1-panel').classList.remove('active');
-		document.querySelector('.player-0-panel').classList.add('active');
-	}	
+
+	badRolls();
 }
+
 function hold(){
 
 if(isPlayerOne){
@@ -54,12 +49,10 @@ if(isPlayerOne){
 	current=0;
 	document.getElementById('current-0').innerHTML=current;
 
-	var isGameEnded = checkWin();
+	checkWin();	
 
-		isPlayerOne=!isPlayerOne;
 		if (!endGame) {
-		document.querySelector('.player-0-panel').classList.remove('active');
-		document.querySelector('.player-1-panel').classList.add('active');
+			nextPlayer();
 		}
 }
 else {
@@ -104,11 +97,36 @@ function changeImage() {
 		case 6:
 		document.getElementById('image').src='dice-6.png';
 	}
+
+	switch (dice2) {
+		case 1:
+			document.getElementById('image2').src = 'dice-1.png';
+			break;
+
+		case 2:
+			document.getElementById('image2').src = 'dice-2.png';
+			break;
+
+		case 3:
+			document.getElementById('image2').src = 'dice-3.png';
+			break;
+
+		case 4:
+			document.getElementById('image2').src = 'dice-4.png';
+			break;
+
+		case 5:
+			document.getElementById('image2').src = 'dice-5.png';
+			break;
+
+		case 6:
+			document.getElementById('image2').src = 'dice-6.png';
+	}
 }
 
 function checkWin(){
 
-	if (isPlayerOne && score1 >= 100) {
+	if (isPlayerOne && score1 >= document.getElementById("points").value ) {
 
 		document.getElementById('name-0').innerHTML = 'Winner!';
 		document.querySelector('.player-0-panel').classList.remove('active');
@@ -118,7 +136,7 @@ function checkWin(){
 		endGame=true;
 	}
 	
-	else if (!isPlayerOne && score2 >= 100) {
+	else if (!isPlayerOne && score2 >= document.getElementById("points").value) {
 
 		document.querySelector('.player-1-panel').classList.remove('active');
 		document.querySelector('.player-1-panel').classList.add('winner');
@@ -129,4 +147,48 @@ function checkWin(){
 		endGame=true;
 	}
 
+}
+
+function badRolls() {
+
+	if ((dice == 1 || dice2 == 1) && isPlayerOne) {
+		current = 0;
+		document.getElementById('current-0').innerHTML = current;
+		nextPlayer();
+	}
+	else if ((dice == 1 || dice2 == 1) && !isPlayerOne) {
+		current = 0;
+		document.getElementById('current-1').innerHTML = current;
+		nextPlayer();
+	}
+		
+	else if (dice == 6 && dice2 == 6) {
+		if (isPlayerOne) {
+			score1 = 0;
+			document.getElementById('score-0').innerHTML = score1;
+			current = 0;
+			document.getElementById('current-0').innerHTML = current;
+			nextPlayer();
+		}
+		else {
+			score2 = 0;
+			document.getElementById('score-1').innerHTML = score2;
+			current = 0;
+			document.getElementById('current-1').innerHTML = current;
+			nextPlayer();
+        }
+    }
+	
+}
+
+function nextPlayer() {
+	if (isPlayerOne) {
+		document.querySelector('.player-0-panel').classList.remove('active');
+		document.querySelector('.player-1-panel').classList.add('active');
+	}
+	else {
+		document.querySelector('.player-1-panel').classList.remove('active');
+		document.querySelector('.player-0-panel').classList.add('active');
+	}
+	isPlayerOne = !isPlayerOne;
 }
