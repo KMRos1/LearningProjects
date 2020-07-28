450e11f4994333ab44077f37f855ae4d1d549345
Ros1 <kamil.m.ros1@gmail.com>
2020-07-28 17:55:25 +0200

    Fixet bug with double declaring

--- 4.Recipes/src/js/index.js
+++ 4.Recipes/src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 
 import x from './test';
+const xx = 23;
 
-console.log(` I imported ${x} from another file and server should reload it automaticaly`);
+console.log(` I imported ${x} from another file and server should reload it automaticaly and the constant is ${xx}`);
